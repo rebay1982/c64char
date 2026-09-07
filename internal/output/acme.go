fee9d215c0859393b50dcfa00160c38d1ad3d5e8
Reda Baydoun <reda.baydoun@gmail.com>
2026-09-07 10:42:11 -0400

    fix: fix indentation

--- internal/output/acme.go
+++ internal/output/acme.go
@@ -15,7 +15,7 @@ func (f acmeFormatter) Output(buf []byte) string {
 		fmt.Fprintf(&builder, "; CHAR %d\n", i)
 
 		for j := range 8 {
-			fmt.Fprintf(&builder, "!byte %%%08b\n", buf[(i<<3)+j])
+			fmt.Fprintf(&builder, "\t!byte %%%08b\n", buf[(i<<3)+j])
 		}
 
 		fmt.Fprintln(&builder)
